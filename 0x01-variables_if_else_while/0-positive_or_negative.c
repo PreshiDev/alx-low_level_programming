4d22354eb542f7678cc09b4f00b6d6bc621f34fa
Preshie-Dev <88434645+PreshiDev@users.noreply.github.com>
2022-03-12 01:25:25 -0800

    C program on positive numbers assigned to a variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -6,9 +6,9 @@
 int main(void)
 {
 	int n;
-	strand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
-	return (0);
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
