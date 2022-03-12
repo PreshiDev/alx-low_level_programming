aa29f025b3bbcf6667ce90be84e6082e4bd16347
Preshie-Dev <88434645+PreshiDev@users.noreply.github.com>
2022-03-12 02:50:21 -0800

    A program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ * */
+int main(void)
+{
+	char letter[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	
+	for (i = 0; i <= 51; i++)
+	{
+		putchar(letter[i]);
+	}
+	putchar("\n");
+	return (0);
+}
