6481fa2b6922761850916e761025eab2a8d3634a
Preshie-Dev <88434645+PreshiDev@users.noreply.github.com>
2022-03-16 06:10:36 -0700

    a function that checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,20 @@
-#include "main.h"
+#include "header.h"
 /**
- * _islower  - A function that checks for lowercase character.
- *@c: the character to be checked
- * Return: 1 if c is lower, 0 otherwise
-*/
-int _islower(int c)
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+    
 }
