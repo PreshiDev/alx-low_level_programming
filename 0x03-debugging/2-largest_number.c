6001f4978afbe3711ca7d369103002487686e914
Preshie-Dev <88434645+PreshiDev@users.noreply.github.com>
2022-03-19 02:04:26 -0700

    correctly prints out the largest of three numbers, no matter the case

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -12,17 +12,27 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
+	if (a > b)
 	{
+		if (b > c)
+			largest = a;
+		else if (a > c)
+			largest = a;
+		else
+			largest = c;
+	}
+	else if (b > a)
+	{
+		if (a > c)
+			largest = b;
+		else if (b > c)
+			largest = b;
+		else
+			largest = c;
+	}
+	else if (c > a)
+		largest = c;
+	else
 		largest = a;
-	}
-	if (b >= a && b >= c)
-	{
-		largest = b;
-	}
-	if (c >= a && c >= b)
-	{
-		largest =  c;
-	}
 	return (largest);
 }
