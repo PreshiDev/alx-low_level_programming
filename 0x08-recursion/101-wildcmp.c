32a3109ce534434e821467059b20bffc3068f30f
Preshie-Dev <88434645+PreshiDev@users.noreply.github.com>
2022-03-30 08:23:08 -0700

    a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 int check_path(char *S1, char *s2);
 char *move_char(char *s2);
@@ -67,4 +68,20 @@ char *move_char(char *s2)
 		return (move_char(s2 + 1));
 	else
 		return (s2);
+}
+
+int wildcmp(char *, char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = wildcmp("school-school.c", "sch*ool.c");
+	printf("%d\n", r);
+	return (0);
 }
