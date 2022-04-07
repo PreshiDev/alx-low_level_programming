f77d2cec58b609f3d037caa458fa3db8e5694372
Preshie-Dev <88434645+PreshiDev@users.noreply.github.com>
2022-04-07 12:08:41 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,17 +1,44 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to copy from s2
  *
- * Return: pointer to the allocated memory
+ * Return: pointer to the newly created string
  */
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *m;
+	unsigned int i = 0, j = 0;
+	char *a;
 
-	m = malloc(b);
-	if (m == NULL)
-		exit(98);
-	return (m);
+	if (s1 == NULL && s2 == NULL)
+	{
+		a = malloc(1);
+		*a = '\0';
+		return (a);
+	}
+	if (s2 != NULL)
+		for (a = s2; i < n && *a != '\0'; a++)
+			i++;
+	if (s1 != NULL)
+		for (a = s1; *a != '\0'; a++)
+			i++;
+	i++;
+	a = malloc(sizeof(*a) * i);
+	if (a == NULL)
+		return (NULL);
+	if (s1 != NULL)
+		for (; *s1 != '\0'; s1++)
+			a[j++] = *s1;
+	if (s2 != NULL)
+		for (; n > 0 && *s2 != '\0'; s2++)
+		{
+			a[j++] = *s2;
+			n--;
+		}
+	for (; j < i; j++)
+		a[j] = '\0';
+	return (a);
 }
